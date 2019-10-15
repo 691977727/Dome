311b11c0423ccef547acb4bd90b9a7c4e4711424
wangshengguo <691977727@qq.com>
2019-10-15 10:37:30 +0800

    提交

---
+++ src/com/wsg/Test.java
@@ -0,0 +1,9 @@
+package com.wsg;
+
+public class Test {
+    public void show(){
+        System.out.println("show()");
+        System.out.println("zaicixinzeng");
+        System.out.println("xinzeng");
+    }
+}
